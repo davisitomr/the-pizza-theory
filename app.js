710df60b6b6c17dc35db0f811681fe9bfd199bb4
davisitomr <dave.munoz@outlook.es>
2022-11-10 09:58:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const harina ="harina integral de centeno";
+const pepperoni ="pepperoni";
+const masa ="masa";
+const tomate ="salsa de tomate";
+const queso ="mozzarrella";
+const champis="champiñones";
